141f4eb6849a10d1d6a7c8a73cc1747d72d48536
Marco Giannini <marco.giannini@6emme.it>
2019-09-30 17:26:35 +0200

    Refresh della posizione del cibo al gameover

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 6 */
-class Screen{
-    constructor(){
+class Screen {
+    constructor() {
         this.screen = document.getElementById('screen');
         this.dialog = document.getElementById('dialog');
         this.stop = false;
@@ -16,26 +16,26 @@ class Screen{
         this.build();
     }
 
-    build(){
-        this.screen.style.height = this.height+'px';
-        this.screen.style.width = this.width+'px';
+    build() {
+        this.screen.style.height = this.height + 'px';
+        this.screen.style.width = this.width + 'px';
     }
 
-    gameover(clockId){
+    gameover(clockId) {
         this.dialog.style.display = 'block';
         clearInterval(clockId);
         this.stop = true;
     }
 }
 
-class Food{
-    constructor(){
+class Food {
+    constructor() {
         this.x = '';
         this.y = '';
         this.create();
     }
 
-    create(){
+    create() {
         let screen = document.getElementById('screen');
 
         this.x = random(0, parseInt(screen.style.width));
@@ -43,14 +43,14 @@ class Food{
         let food = document.createElement('div');
 
         food.id = 'food';
-        food.style.left = this.x+'px';
-        food.style.top = this.y+'px';
+        food.style.left = this.x + 'px';
+        food.style.top = this.y + 'px';
         screen.appendChild(food);
     }
 }
 
-class Snake{
-    constructor(){
+class Snake {
+    constructor() {
         this.x = 0;
         this.y = 0;
         this.xspeed = 1;
@@ -60,21 +60,21 @@ class Snake{
         this.snake = '';
         this.point = 0;
         this.createSnake();
-        for(let i = 0; i < 3; i++){
+        for (let i = 0; i < 3; i++) {
             this.expand(false);
         }
         this.moveSnake(this.xspeed, this.yspeed);
         this.updatePoint();
     }
 
-    updatePoint(){
+    updatePoint() {
         let point = document.getElementById('point');
         point.innerText = this.point;
     }
 
-    createSnake(){
+    createSnake() {
         let snake = document.getElementById('snake');
-        while(snake.firstChild){
+        while (snake.firstChild) {
             snake.removeChild(snake.firstChild);
         }
         let head = document.createElement('div');
@@ -83,38 +83,38 @@ class Snake{
         this.snake = head;
     }
 
-    moveSnake(xspeed, yspeed){
-        if(xspeed){
-            if(!this.xspeed){
+    moveSnake(xspeed, yspeed) {
+        if (xspeed) {
+            if (!this.xspeed) {
                 this.xspeed = xspeed;
                 this.yspeed = 0;
             }
         }
-        if(yspeed){
-            if(!this.yspeed){
+        if (yspeed) {
+            if (!this.yspeed) {
                 this.xspeed = xspeed;
                 this.yspeed = yspeed;
             }
         }
     }
 
-    expand(updatePoint = true){
+    expand(updatePoint = true) {
         let snake = document.getElementById('snake');
         let tail = document.createElement('div');
         tail.classList.add('tail');
         tail.style.left = '-100px';
         tail.style.top = '-100px';
         snake.appendChild(tail);
-        if(updatePoint){
+        if (updatePoint) {
             this.point++;
             this.updatePoint();
         }
     }
 
-    follow(){
+    follow() {
         let tail = document.querySelectorAll('.tail');
-        for(let i = 0; i < tail.length; i++){
-            if(i == 0){
+        for (let i = 0; i < tail.length; i++) {
+            if (i == 0) {
                 tail[i].dataset.left = tail[i].style.left;
                 tail[i].dataset.top = tail[i].style.top;
                 tail[i].style.left = this.snake.dataset.left;
@@ -122,42 +122,42 @@ class Snake{
             } else {
                 tail[i].dataset.left = tail[i].style.left;
                 tail[i].dataset.top = tail[i].style.top;
-                tail[i].style.left = tail[i-1].dataset.left;
-                tail[i].style.top = tail[i-1].dataset.top;
+                tail[i].style.left = tail[i - 1].dataset.left;
+                tail[i].style.top = tail[i - 1].dataset.top;
             }
         }
     }
 
-    isGameover(game){
+    isGameover(game) {
         let head = document.querySelector('.head');
         let tail = document.querySelectorAll('.tail');
         let died = false;
-        tail.forEach(function(piece){
-            if((piece.style.left === head.style.left) && (piece.style.top === head.style.top)){
+        tail.forEach(function (piece) {
+            if ((piece.style.left === head.style.left) && (piece.style.top === head.style.top)) {
                 died = true;
             }
         });
-        if(this.x === -10 || this.x == game.width || this.y === -10 || this.y == game.height){
+        if (this.x === -10 || this.x == game.width || this.y === -10 || this.y == game.height) {
             died = true;
         }
-        if(!died){
+        if (!died) {
             return false;
         } else {
             return true;
         }
     }
 
-    update(){
-        this.snake.dataset.left = this.x+'px';
-        this.snake.dataset.top = this.y+'px';
+    update() {
+        this.snake.dataset.left = this.x + 'px';
+        this.snake.dataset.top = this.y + 'px';
         this.x = this.x + this.xspeed * this.scl;
         this.y = this.y + this.yspeed * this.scl;
-        this.snake.style.left = this.x+'px';
-        this.snake.style.top = this.y+'px';
+        this.snake.style.left = this.x + 'px';
+        this.snake.style.top = this.y + 'px';
     }
 
-    eat(food){
-        if((snake.x == food.x) && (snake.y == food.y)){
+    eat(food) {
+        if ((snake.x == food.x) && (snake.y == food.y)) {
             document.getElementById('food').remove();
             snake.expand();
             food.create();
@@ -169,19 +169,19 @@ let snake = new Snake();
 let game = new Screen();
 let food = new Food();
 
-function onKeyPress(keyCode){
-    if(keyCode == 'ArrowUp'){
+function onKeyPress(keyCode) {
+    if (keyCode == 'ArrowUp') {
         snake.moveSnake(0, -1);
-    } else if(keyCode == 'ArrowDown'){
+    } else if (keyCode == 'ArrowDown') {
         snake.moveSnake(0, 1);
-    } else if(keyCode == 'ArrowLeft'){
+    } else if (keyCode == 'ArrowLeft') {
         snake.moveSnake(-1, 0);
-    } else if(keyCode == 'ArrowRight'){
+    } else if (keyCode == 'ArrowRight') {
         snake.moveSnake(1, 0);
-    } else if(keyCode == 'Escape'){
+    } else if (keyCode == 'Escape') {
         snake.expand();
-    } else if(keyCode == 'Space'){
-        if(game.stop){
+    } else if (keyCode == 'Space') {
+        if (game.stop) {
             snake = new Snake();
             game = new Screen();
             clock = play();
@@ -189,28 +189,30 @@ function onKeyPress(keyCode){
     }
 }
 
-document.addEventListener('keyup', function(e){
+document.addEventListener('keyup', function (e) {
     onKeyPress(e.code);
 });
 
 function random(min, max) {
-    return (Math.floor(Math.random() * ((max - min) / 10 )) + min) * 10;
+    return (Math.floor(Math.random() * ((max - min) / 10)) + min) * 10;
 }
 
-function play(){
-    let interval = setInterval( function(){
+function play() {
+    let interval = setInterval(function () {
         snake.update();
         snake.follow();
         snake.eat(food);
-        if(snake.isGameover(game)){
+        if (snake.isGameover(game)) {
+            document.getElementById('food').remove();
+            food.create();
             game.gameover(interval);
         }
     }, game.refreshRate);
 }
 
-document.querySelectorAll('.btn-group button').forEach(function(v){
-    v.addEventListener('click', function(){
-        this.parentElement.querySelectorAll('button').forEach(function(v){ v.classList.remove('selected'); });
+document.querySelectorAll('.btn-group button').forEach(function (v) {
+    v.addEventListener('click', function () {
+        this.parentElement.querySelectorAll('button').forEach(function (v) { v.classList.remove('selected'); });
         this.classList.add('selected');
     });
 });
